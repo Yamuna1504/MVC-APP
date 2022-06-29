71172fd19e160ca8da3cb7e2d48326fb80f3be27
003U5Z744 <003U5Z744@IBM-75S2KN3>
2022-06-29 10:21:17 +0530

    Add project files.

---
+++ MVC APP/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace MVC_APP.Models
+{
+    public class User
+    {
+        String UserName { get; set; }
+        String Password { get; set; }
+        String Email { get; set; }
+
+
+    }
+}
---
+++ MVC APP/Models/dbconnection.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_APP.Models
+{
+    public class dbconnection
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+            
+
+    }
+}
